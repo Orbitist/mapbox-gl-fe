f022c23f71c37c74df24a4225a13bd396daad52b
Nicholas Gunner <gunnernr@gmail.com>
2016-07-21 07:57:55 -0400

    added list item div

--- js/maplist.js
+++ js/maplist.js
@@ -5,8 +5,8 @@ $(document).ready(
             function(data){
                 // ciclo l'array
                 for(i=0; i<data.length; i++){
-                    var  listcontent  = '<p id="' + data[i].list_point_id + '">' + data[i].list_title + '</p><script>document.getElementById("' + data[i].list_point_id + '").addEventListener("click", function () {map.flyTo({center: [' + data[i].list_longitude + ',' + data[i].list_latitude + '],zoom: 15,bearing: 360 * Math.random(),pitch: 60});});</script>';
-                    $('div.map-list').append(listcontent);
+                    var  listItem  = '<div class="map-list-item" id="' + data[i].list_point_id + '""><p>' + data[i].list_title + '</p></div><script>document.getElementById("' + data[i].list_point_id + '").addEventListener("click", function () {map.flyTo({center: [' + data[i].list_longitude + ',' + data[i].list_latitude + '],zoom: 15,bearing: 360 * Math.random(),pitch: 60});});</script>';
+                    $('div.map-list').append(listItem);
                 }
             }
         );
