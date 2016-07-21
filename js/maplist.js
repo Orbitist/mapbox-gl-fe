41b84f1e5cac3ba9edb1bcf4fb4e16dd48e4f94d
Nicholas Gunner <gunnernr@gmail.com>
2016-07-21 07:52:07 -0400

    working on the map list

--- js/maplist.js
+++ js/maplist.js
@@ -5,7 +5,7 @@ $(document).ready(
             function(data){
                 // ciclo l'array
                 for(i=0; i<data.length; i++){
-                    var  listcontent  = '<p>' + data[i].list_title + '</p>';
+                    var  listcontent  = '<p id="' + data[i].list_point_id + '">' + data[i].list_title + '</p><script>document.getElementById("' + data[i].list_point_id + '").addEventListener("click", function () {map.flyTo({center: [' + data[i].list_longitude + ',' + data[i].list_latitude + '],zoom: 15,bearing: 360 * Math.random(),pitch: 60});});</script>';
                     $('div.map-list').append(listcontent);
                 }
             }
