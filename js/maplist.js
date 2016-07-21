3f262297f46e032681e9f4571bbb54ad738cca6e
Nicholas Gunner <gunnernr@gmail.com>
2016-07-21 13:48:45 -0400

    CLEANING code

--- js/maplist.js
+++ js/maplist.js
@@ -1,14 +1,14 @@
 $(document).ready(
-    function(){
-        $.getJSON(
-            'https://app.orbitist.com/api/v1/list.json?mapid=' + mapid,
-            function(data){
-                // ciclo l'array
-                for(i=0; i<data.length; i++){
-                    var  listItem  = '<div class="map-list-item" id="' + data[i].list_point_id + '""><p>' + data[i].list_title + '</p></div><script>document.getElementById("' + data[i].list_point_id + '").addEventListener("click", function () {map.flyTo({center: [' + data[i].list_longitude + ',' + data[i].list_latitude + '],zoom: 15,bearing: 360 * Math.random(),pitch: 60});});</script>';
-                    $('div.map-list').append(listItem);
-                }
-            }
-        );
-    }
+  function(){
+    $.getJSON(
+      'https://app.orbitist.com/api/v1/list.json?mapid=' + mapid,
+      function(data){
+        // ciclo l'array
+        for(i=0; i<data.length; i++){
+          var  listItem  = '<div class="map-list-item" id="' + data[i].list_point_id + '""><p>' + data[i].list_title + '</p></div><script>document.getElementById("' + data[i].list_point_id + '").addEventListener("click", function () {map.flyTo({center: [' + data[i].list_longitude + ',' + data[i].list_latitude + '],zoom: 15,bearing: 360 * Math.random(),pitch: 60});});</script>';
+          $('div.map-list').append(listItem);
+        }
+      }
+    );
+  }
 );
