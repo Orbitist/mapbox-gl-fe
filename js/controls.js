db63f5c5f3662253b1198aed153d56ddcd89ced6
Nicholas Gunner <gunnernr@gmail.com>
2016-07-22 19:47:59 -0400

    styled the map list

--- js/controls.js
+++ js/controls.js
@@ -29,6 +29,13 @@ jQuery(document).ready(function () {
       jQuery('.map-info-toggle').css({"right": "10px"}),
       jQuery('.map-list-toggle').css({"left": "10px"})
     });
+    jQuery('.map-list').click(function () {
+      jQuery('.map').css({"left": "0"}),
+      jQuery('.map-list').css({"left": "-100%"}),
+      jQuery('.map-toggle-list').css({"right": "calc(100% + 10px)"}),
+      jQuery('.map-info-toggle').css({"right": "10px"}),
+      jQuery('.map-list-toggle').css({"left": "10px"})
+    });
   }
 });
 
