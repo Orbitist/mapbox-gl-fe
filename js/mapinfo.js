bb820ef7a26e952ff5f8566dea857465e9b4b03e
Nicholas Gunner <gunnernr@gmail.com>
2016-07-25 23:34:24 -0400

    added google analytics

--- js/mapinfo.js
+++ js/mapinfo.js
@@ -23,6 +23,23 @@ $(document).ready(
                     $('head').append('<style>' + customcss + '</style>');
                     mapboxgl.accessToken = data[i].map_mapbox_access_token;
                     map.setStyle(data[i].map_mapbox_style);
+                    //Google Analytics
+                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                    //If user submitted google analytics cods
+                  	if ( googleanalytics.length > 5 ) {
+                  		ga('create', googleanalytics, 'auto', {'name':'b'});
+                  		ga('create', 'UA-50308061-3', 'auto');
+                  		ga('send', 'pageview');
+                  		ga('b.send', 'pageview');
+                  	}
+                  	//Otherwise just load the Orbitist Tracking code
+                  	else {
+                  		ga('create', 'UA-50308061-3', 'auto');
+                  		ga('send', 'pageview');
+                  	}
                 }
             }
         );
