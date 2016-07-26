a8c91913c70be3cee71a2e82b7efd432c6a21728
Nicholas Gunner <gunnernr@gmail.com>
2016-07-25 23:58:29 -0400

    Added support for custom map box layer

--- js/mapinfo.js
+++ js/mapinfo.js
@@ -13,24 +13,24 @@ $(document).ready(
                          content  += '</h3>';
                          content +=  data[i].map_body;
                          content +=  '</div>';
-                         cartodbkey =  data[i].map_cartodb;
-                         basemapurl =  data[i].map_tiles;
-                         mapMapboxAccessToken = data[i].map_mapbox_access_token;
-                         custombasemapurl = data[i].map_custom_tiles;
-                         customcss = data[i].map_css;
-                         googleanalytics = data[i].map_google_analytics;
-                    $('div.map-info').append(content);
-                    $('head').append('<style>' + customcss + '</style>');
-                    mapboxgl.accessToken = data[i].map_mapbox_access_token;
-                    map.setStyle(data[i].map_mapbox_style);
+                    $('div.map-info').append(content); // Add content to map-info pane
+                    $('head').append('<style>' + data[i].map_css + '</style>'); // Add custom styles to map
+                    if ( data[i].map_custom_mapbox_access_token.length > 5 ) {
+                      mapboxgl.accessToken = data[i].map_custom_mapbox_access_token;
+                      map.setStyle(data[i].map_custom_mapbox_style);
+                    }
+                    else {
+                      mapboxgl.accessToken = data[i].map_mapbox_access_token;
+                      map.setStyle(data[i].map_mapbox_style);
+                    }
                     //Google Analytics
                     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
                     //If user submitted google analytics cods
-                  	if ( googleanalytics.length > 5 ) {
-                  		ga('create', googleanalytics, 'auto', {'name':'b'});
+                  	if ( data[i].map_google_analytics.length > 5 ) {
+                  		ga('create', data[i].map_google_analytics, 'auto', {'name':'b'});
                   		ga('create', 'UA-50308061-3', 'auto');
                   		ga('send', 'pageview');
                   		ga('b.send', 'pageview');
