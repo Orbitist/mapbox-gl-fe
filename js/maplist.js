38be2b8d82aab3f9bff989ebe6107f7722baa258
Nicholas Gunner <gunnernr@gmail.com>
2016-07-27 13:54:18 -0400

    updated zoom on click

--- js/maplist.js
+++ js/maplist.js
@@ -7,7 +7,7 @@ $(document).ready(
         $('div.map-list').append(mapReset);
         // ciclo l'array
         for(i=0; i<data.length; i++){
-          var  listItem  = '<hr><div class="map-list-item" id="' + data[i].list_point_id + '"><img src="' + data[i].list_image + '" /><p>' + data[i].list_title + '</p></div><script>document.getElementById("' + data[i].list_point_id + '").addEventListener("click", function () {map.flyTo({center: [' + data[i].list_longitude + ',' + data[i].list_latitude + '],zoom: 15,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
+          var  listItem  = '<hr><div class="map-list-item" id="' + data[i].list_point_id + '"><img src="' + data[i].list_image + '" /><p>' + data[i].list_title + '</p></div><script>document.getElementById("' + data[i].list_point_id + '").addEventListener("click", function () {map.flyTo({center: [' + data[i].list_longitude + ',' + data[i].list_latitude + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
           $('div.map-list').append(listItem);
         }
       }
